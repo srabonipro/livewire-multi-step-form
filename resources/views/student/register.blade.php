299eae9405dbe560a33879c9e8b19dc36f493348
sraboni303 <sraboni391@gmail.com>
2021-09-10 22:11:12 +0600

    livewire setup

---
+++ resources/views/student/register.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Student Register</title>
+    @livewireStyles
+</head>
+<body>
+        <h1>Student Register</h1>
+
+    @livewireScripts
+</body>
+</html>
