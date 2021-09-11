d94ea6c7c76fcb0cc31897b7099d327243a33174
srabonipro <sraboni391@gmail.com>
2021-09-11 11:23:44 +0600

    validate multi step form

--- app/Http/Livewire/StudentRegister.php
+++ app/Http/Livewire/StudentRegister.php
@@ -25,6 +25,8 @@ class StudentRegister extends Component
 
     public function increaseStep()
     {
+        $this->resetErrorBag();
+        $this->validateData();
         $this->currentStep++;
         if ($this->currentStep > $this->totalSteps) {
             $this->currentStep = $this->totalSteps;
@@ -33,9 +35,45 @@ class StudentRegister extends Component
 
     public function decreaseStep()
     {
+        $this->resetErrorBag();
         $this->currentStep--;
         if ($this->currentStep < 1) {
             $this->currentStep = 1;
         }
+    }
+
+    public function validateData()
+    {
+        if ($this->currentStep == 1) {
+            $this->validate([
+                'first_name' => 'required|string',
+                'last_name' => 'required|string',
+                'gender' => 'required',
+                'age' => 'required|digits:2'
+            ]);
+        } elseif ($this->currentStep == 2) {
+            $this->validate([
+                'email' => 'required|email|unique:students,email',
+                'phone' => 'required',
+                'country' => 'required',
+                'city' => 'required',
+            ]);
+        } elseif ($this->currentStep == 3) {
+            $this->validate([
+                'frameworks' => 'required|array|min:2|max:3'
+            ]);
+        }
+    }
+
+    public function register()
+    {
+        $this->resetErrorBag();
+        if ($this->currentStep == 4) {
+            $this->validate([
+                'cv' => 'required|mimes:docx,pdf|max:1024',
+                'terms' => 'accepted'
+            ]);
+        }
+        dd('Now, you can submit form');
     }
 }
